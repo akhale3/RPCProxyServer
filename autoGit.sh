50e41f179d4b1f9ccc43feb0d0253b793ca7f634
Anish Khale <anish_khale@hotmail.com>
2014-03-25 09:19:08 -0400

    Test

---
+++ autoGit.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Check status, pull, add, commit, check status, push, show log for git
+clear
+echo "Current status."
+echo ""
+git status
+echo "Pulling latest code from github."
+echo ""
+git pull
+echo "Adding files to commit."
+echo ""
+git add .
+read -p "Please enter a commit message: " msg
+echo ""
+echo "Committing."
+echo ""
+git commit -m $msg
+echo ""
+echo "Pushing code to github"
+echo ""
+git push origin
